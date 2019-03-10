43642e5dea9de6991d0ec160e09c0195e5dad7a4
Aditya Sinha <31968928+jrsinha2@users.noreply.github.com>
2019-03-10 20:50:46 +0530

    Added pause/resume, start menu functionalities.

--- src/Game.py
+++ src/Game.py
@@ -6,10 +6,11 @@ class Game:
     def __init__(self,display):  #constructor
         self.display = display
         self.score = 0
+        self.pause = False
     
     def start_screen(self):
         clock = pygame.time.Clock() #get time (initializing time)
-        self.display.fill(Config['colors']['green']) #wipe old position  
+        self.display.fill(Config['colors']['brown']) #wipe old position  
         pygame.draw.rect(self.display,Config['colors']['black'],[ 
                 Config['game']['bumper_size'],
                 Config['game']['bumper_size'],
@@ -22,7 +23,7 @@ class Game:
         start_text_rect = start_text.get_rect(
                 center = (
                     Config['game']['width']/2,   #width of box
-                    Config['game']['height']/2  #height of box
+                    Config['game']['height']/4  #height of box
                 )
             )
         self.display.blit(start_text,start_text_rect)
@@ -33,8 +34,51 @@ class Game:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
                         self.loop()
-                
-                    
+            mouse = pygame.mouse.get_pos()
+            start_menu_pos_x = Config['game']['width']/2
+            start_menu_pos_y = 4*Config['game']['height']/8
+            start_menu_height = Config['game']['height']*0.1
+            start_menu_width =  Config['game']['width']*0.25
+            start_menu_text = "Play"
+            
+            quit_menu_pos_x = Config['game']['width']/2
+            quit_menu_pos_y = 6*Config['game']['height']/8
+            quit_menu_height = Config['game']['height']*0.1
+            quit_menu_width =  Config['game']['width']*0.25
+            quit_menu_text = "Quit"
+            
+            #interactive buttons
+            self.button(start_menu_text,start_menu_pos_x,start_menu_pos_y,start_menu_width,start_menu_height,Config['colors']['green'],Config['colors']['bright_green'],Config['colors']['white'],self.loop)
+            self.button(quit_menu_text,quit_menu_pos_x,quit_menu_pos_y,quit_menu_width,quit_menu_height,Config['colors']['red'],Config['colors']['bright_red'],Config['colors']['white'],exit)
+            
+            '''if start_menu_pos_x + start_menu_width/2 > mouse[0] > start_menu_pos_x-start_menu_width/2 and  start_menu_pos_y + start_menu_height/2 > mouse[1] > start_menu_pos_y- start_menu_height/2 :
+                pygame.draw.rect(self.display,Config['colors']['bright_green'],
+                (start_menu_pos_x-start_menu_width/2,start_menu_pos_y- start_menu_height/2,start_menu_width,start_menu_height))
+            else:
+                pygame.draw.rect(self.display,Config['colors']['green'],
+                (start_menu_pos_x-start_menu_width/2,start_menu_pos_y- start_menu_height/2,start_menu_width,start_menu_height))
+            
+            if quit_menu_pos_x + quit_menu_width/2 > mouse[0] > quit_menu_pos_x-quit_menu_width/2 and  quit_menu_pos_y + quit_menu_height/2 > mouse[1] > quit_menu_pos_y- quit_menu_height/2 :
+                pygame.draw.rect(self.display,Config['colors']['bright_red'],
+                (quit_menu_pos_x-quit_menu_width/2,quit_menu_pos_y- quit_menu_height/2,quit_menu_width,quit_menu_height))
+            else:
+                pygame.draw.rect(self.display,Config['colors']['red'],
+                (quit_menu_pos_x-quit_menu_width/2,quit_menu_pos_y- quit_menu_height/2,quit_menu_width,quit_menu_height))
+            
+            #displaying text on buttons
+            start_menu_text_object = font.render(start_menu_text,True,Config['colors']['white'])
+            start_menu_text_rect = start_menu_text_object.get_rect(
+                center = (start_menu_pos_x,start_menu_pos_y)
+            )
+            self.display.blit(start_menu_text_object, start_menu_text_rect)
+
+            quit_menu_text_object = font.render(quit_menu_text,True,Config['colors']['white'])
+            quit_menu_text_rect = quit_menu_text_object.get_rect(
+                center = (quit_menu_pos_x,quit_menu_pos_y)
+            )
+            self.display.blit(quit_menu_text_object, quit_menu_text_rect)
+            '''
+
             pygame.display.update()
             clock.tick(Config['game']['fps']) #to slow time rate (30 frames per second)
     
@@ -53,6 +97,10 @@ class Game:
                 if event.type == pygame.QUIT:   #type such as mouse,keyboard,exit the application
                     exit()
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.pause = True
+                        self.paused(Config['game']['width']/2,Config['game']['height']/4,Config['game']['width']*0.25,Config['game']['height']*0.1,Config['colors']['white'])
+                        #print("Came out of pause")
                     if event.key == pygame.K_LEFT:
                         x_change  = -Config['snake']['speed']
                         y_change = 0
@@ -66,7 +114,7 @@ class Game:
                         y_change = Config['snake']['speed']
                         x_change = 0
             
-            self.display.fill(Config['colors']['green']) #wipe old position  
+            self.display.fill(Config['colors']['brown']) #wipe old position  
             pygame.draw.rect(self.display,Config['colors']['black'],[ 
                 Config['game']['bumper_size'],
                 Config['game']['bumper_size'],
@@ -123,7 +171,7 @@ class Game:
 
     def end_screen(self,score):
         clock = pygame.time.Clock() #get time (initializing time)
-        self.display.fill(Config['colors']['green']) #wipe old position  
+        self.display.fill(Config['colors']['brown']) #wipe old position  
         pygame.draw.rect(self.display,Config['colors']['black'],[ 
                 Config['game']['bumper_size'],
                 Config['game']['bumper_size'],
@@ -147,8 +195,57 @@ class Game:
                     exit()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
-                        self.loop()
+                        self.start_screen()
                     
             pygame.display.update()
             clock.tick(Config['game']['fps']) #to slow time rate (30 frames per second)
     
+    def button(self,text,pos_x,pos_y,width,height,inactive_color,active_color,text_color,action = None):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        
+        if pos_x + width/2 > mouse[0] > pos_x-width/2 and  pos_y + height/2 > mouse[1] > pos_y- height/2 :
+            pygame.draw.rect(self.display,active_color,
+            (pos_x-width/2,pos_y- height/2,width,height))
+            if click[0] == 1 and action != None:
+                action()
+        else:
+            pygame.draw.rect(self.display,inactive_color,
+            (pos_x-width/2,pos_y- height/2,width,height))
+        
+        pygame.font.init()
+        font = pygame.font.Font('./assets/Now-Regular.otf',28)
+        start_menu_text_object = font.render(text,True,text_color)
+        start_menu_text_rect = start_menu_text_object.get_rect(
+                    center = (pos_x,pos_y)
+                )
+        self.display.blit(start_menu_text_object, start_menu_text_rect)
+
+    def paused(self,pos_x,pos_y,width,height,text_color):
+        
+        pygame.font.init()
+        font = pygame.font.Font('./assets/Now-Regular.otf',28)
+        start_menu_text_object = font.render("Paused",True,text_color)
+        start_menu_text_rect = start_menu_text_object.get_rect(
+                    center = (pos_x,pos_y)
+                )
+        self.display.blit(start_menu_text_object, start_menu_text_rect)
+        clock = pygame.time.Clock()
+        while self.pause ==True :
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN or event.key == pygame.K_ESCAPE:
+                        self.unpause()
+            self.button("Quit",pos_x,pos_y+150,width,height,Config['colors']['red'],Config['colors']['bright_red'],text_color,exit)
+            self.button("Continue",pos_x,pos_y+50,width,height,Config['colors']['green'],Config['colors']['bright_green'],text_color,self.unpause)
+            pygame.display.update()
+            clock.tick(15)
+    
+        
+
+    def unpause(self):
+        self.pause = False
+     
